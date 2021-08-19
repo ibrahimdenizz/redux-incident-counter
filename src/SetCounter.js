7866dd7891e19190e491fa68dd30c990f067f925
ibrahim <dnzibrahim1@gmail.com>
2021-08-19 22:12:58 +0300

    Add Hooking It Up With React section

--- src/SetCounter.js
+++ src/SetCounter.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { set } from './actions';
+
 export const SetCounter = () => {
+  const countFromStore = useSelector((state) => state.count);
+  const [newCount, setNewCount] = useState(countFromStore);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setNewCount(countFromStore);
+  }, [countFromStore]);
+
   return (
     <section className="controls">
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          dispatch(set(newCount));
         }}
       >
         <label htmlFor="set-to">Set Count</label>
-        <input id="set-to" type="number" />
+        <input
+          id="set-to"
+          type="number"
+          value={newCount}
+          onChange={(e) => setNewCount(e.target.value)}
+        />
         <input type="submit" />
       </form>
     </section>
